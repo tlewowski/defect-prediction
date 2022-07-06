eb4c6a378cda798bbd804575daf29d0c899e5c5b
Tomasz Lewowski <tomasz.lewowski@ratelware.com>
2022-07-06 18:55:21 +0200

    Formatting JavaMetrics results

--- runners/metric_gathering/javametrics.py
+++ runners/metric_gathering/javametrics.py
@@ -1,12 +1,29 @@
 import os
 import subprocess
+import csv
 
 from iresearch_context import IResearchContext
+from metric_gathering.metric_value import MetricValue
 from metrics_tool import MetricsTool
 from project import Project
 
 class JavaMetrics(MetricsTool):
     name = 'javametrics'
+
+    @staticmethod
+    def make_entity_name(items: list[str]) -> str:
+        assert len(items) == 5, "Expected 4 items, got: {}".format(items)
+        return "-/+/-".join(items)
+
+    @staticmethod
+    def metrics_from_row(row: list[str], header: list[str]) -> list[MetricValue]:
+        entity_name = JavaMetrics.make_entity_name(row[0:5])
+        values = row[5:]
+        names = header[5:]
+        pairs = zip(names, values)
+
+        return [MetricValue("JAVAMETRICS_{}".format(p[0]), entity_name, p[1]) for p in pairs]
+
 
     def __init__(self, javametrics_jar, context: IResearchContext):
         self.javametrics_jar = javametrics_jar
@@ -30,5 +47,16 @@ class JavaMetrics(MetricsTool):
 
         return raw_results_dir
 
-    def normalize_results(self, raw_results_path: str, project: Project) -> str:
-        pass
+    def normalize_results(self, raw_results_path: str, project: Project):
+        all_metrics = []
+        with open(raw_results_path, 'r') as file:
+            result_reader = csv.reader(file, delimiter = ",")
+            headers = next(result_reader)
+            for row in result_reader:
+                all_metrics.extend(JavaMetrics.metrics_from_row(row, headers))
+
+        reports_path = self.context.reports_wd(self, project)
+        target_file = os.path.join(reports_path, "metrics.csv")
+        self.print_final_metrics(target_file, all_metrics)
+
+        print("JAVAMETRICS: Extracted", len(all_metrics), "metrics from", len(raw_results_path))
