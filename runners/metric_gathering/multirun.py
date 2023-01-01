27672a8fdf9a45131c3ba240a3b6772ac9e774db
Tomasz Lewowski <tomasz.lewowski@ratelware.com>
2023-01-01 14:03:28 +0100

    Working running stuff on multiple commits

--- runners/metric_gathering/multirun.py
+++ runners/metric_gathering/multirun.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import time
 
 from git import InvalidGitRepositoryError
 from gitdb.exc import BadName
@@ -24,21 +25,29 @@ def first_commit(repo):
     pass
 
 def validate_commit_range(repo, start, end):
-    start_commit = repo.commit(start)
+    start_commit = None
+    if start is not None:
+        start_commit = repo.commit(start)
+
     end_commit = repo.commit(end)
 
-    if not repo.is_ancestor(start_commit, end_commit):
+    if start is not None and not repo.is_ancestor(start_commit, end_commit):
         raise InvalidGitCommitRange(repo, start, end)
 
     return [start_commit, end_commit]
 
 def get_commit_iterator(repo, args):
-    start = args.start if not args.start is None else first_commit(repo)
+    start = args.start
     end = args.end
 
     start_commit, end_commit = validate_commit_range(repo, start, end)
-    commits = list(repo.iter_commits(rev="{}..{}".format(start, end)))
-    commits.append(start_commit)
+
+    if start_commit is not None:
+        commits = list(repo.iter_commits(rev="{}..{}".format(start, end)))
+        commits.append(start_commit)
+    else:
+        commits = list(repo.iter_commits(rev=end))
+
     return commits
 
 def get_git_repo(path):
@@ -59,8 +68,23 @@ def run_as_main():
         print("MGMAIN_M: Invalid commit range: from ", r.start, " to ", r.end, " in ", args.project_path, " (" + os.path.abspath(args.project_path) + ")")
         exit(1)
 
+    total_commits = len(commits)
+    print("MGMAIN_M: Got a total of " + str(total_commits) + " commits to analyze for ", args.project_path, " (" + os.path.abspath(args.project_path) + ")")
+    current_index = 1
+    start_time = time.monotonic()
     for commit in commits:
-        print("MGMAIN_M: Checking out " + commit.hexsha + " in ", args.project_path, " (" + os.path.abspath(args.project_path) + ")")
+        commit_start_time = time.monotonic()
+        print("MGMAIN_M: Checking out commit " + str(current_index) + "/" + str(total_commits) + " (" + commit.hexsha + ") in ", args.project_path, " (" + os.path.abspath(args.project_path) + ")")
+        repo.head.reference = commit
+        repo.head.reset(index=True, working_tree=True)
+        commit_end_time = time.monotonic()
+        time_taken = commit_end_time - commit_start_time
+        total_time = commit_end_time - start_time
+        avg_commit_time = total_time / current_index
+        print("MGMAIN_M: Finished dealing with commit " + str(current_index) + "/" + str(
+            total_commits) + " (" + commit.hexsha + ") in ", args.project_path,
+              " (" + os.path.abspath(args.project_path) + "). Time taken: current: " + str(time_taken) + "s / total: " + str(total_time) + "s / average: " + str(avg_commit_time) + "s" )
+        current_index = current_index + 1
 
 if __name__ == '__main__':
     run_as_main()
