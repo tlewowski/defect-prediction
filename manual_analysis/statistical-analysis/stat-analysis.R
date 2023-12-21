e3256496b0e62142de9b155b00cf8d1833b0c11e
Tomasz Lewowski <tomasz.lewowski@ratelware.com>
2023-12-21 22:38:15 +0100

    Results for statistical analysis

--- manual_analysis/statistical-analysis/stat-analysis.R
+++ manual_analysis/statistical-analysis/stat-analysis.R
@@ -8,11 +8,14 @@ library(dplyr)
 #setwd("/Users/lma/Documents/...")
 
 # Example statistical analysis for AllMetrics vs PyDriller process metrics for the MCC metric
-data <- readxl::read_excel("./Out_23_AllMetricsVsPyDrillerProcessMetrics.xlsx")#, sheet = "Out_23_AllMetricsVsPyDrillerPro")
+basic_data <- readxl::read_excel("./basic-metrics-full.xlsx")#, sheet = "Out_23_AllMetricsVsPyDrillerPro")
+single_data <- readxl::read_excel("./single-metric-results-full.xlsx")#, sheet = "Out_23_AllMetricsVsPyDrillerPro")
+minimal_data <- readxl::read_excel("./minimal-results-full.xlsx")#, sheet = "Out_23_AllMetricsVsPyDrillerPro")
 
+print("Pydriller vs ALL")
 pHatTest_mcc <- reproducer::PHat.test(
-  x=dplyr::filter(data, metric_set == "pydriller")$real_mcc, 
-  y=dplyr::filter(data, metric_set == "all-non-null-numeric")$real_mcc, 
+  x=dplyr::filter(basic_data, metric_set == "pydriller")$real_mcc, 
+  y=dplyr::filter(basic_data, metric_set == "all-non-null-numeric")$real_mcc, 
   alternative="greater")
 pHatTest_mcc
 # Outout:
@@ -23,8 +26,8 @@ pHatTest_mcc
 
 # Example statistical analysis for AllMetrics vs PyDriller process metrics for the precision
 pHatTest_precision <- reproducer::PHat.test(
-  x=dplyr::filter(data, metric_set == "all-non-null-numeric")$real_precision,  
-  y=dplyr::filter(data, metric_set == "pydriller")$real_precision,
+  y=dplyr::filter(basic_data, metric_set == "all-non-null-numeric")$real_precision,  
+  x=dplyr::filter(basic_data, metric_set == "pydriller")$real_precision,
   alternative="greater")
 pHatTest_precision
 # Outout:
@@ -32,5 +35,69 @@ pHatTest_precision
 #      phat sqse.phat phat.df phat.tvalue phat.pvalue phat.ci.lower phat.ci.upper phat.sig
 #     <dbl>     <dbl>   <dbl>       <dbl>       <dbl>         <dbl>         <dbl> <lgl>   
 #   1 0.679  0.000525    416.        7.80    2.58e-14         0.641             1 TRUE  
+
+
+print("PyDriller vs Process")
+reproducer::PHat.test(
+  x=dplyr::filter(basic_data, metric_set == "process")$real_mcc, 
+  y=dplyr::filter(basic_data, metric_set == "pydriller")$real_mcc, 
+  alternative="greater")
+reproducer::PHat.test(
+  x=dplyr::filter(basic_data, metric_set == "process")$real_precision, 
+  y=dplyr::filter(basic_data, metric_set == "pydriller")$real_precision, 
+  alternative="greater")
+
+
+
+print("Product vs code smells")
+reproducer::PHat.test(
+  x=dplyr::filter(basic_data, metric_set == "none")$real_mcc, 
+  y=(dplyr::filter(basic_data, metric_set == "product") %>% dplyr::filter(smell_models == "False"))$real_mcc, 
+  alternative="greater")
+reproducer::PHat.test(
+  x=dplyr::filter(basic_data, metric_set == "none")$real_precision, 
+  y=(dplyr::filter(basic_data, metric_set == "product") %>% dplyr::filter(smell_models == "False"))$real_precision, 
+  alternative="greater")
+
+
+
+print("With or without code smells")
+reproducer::PHat.test(
+  x=dplyr::filter(basic_data, smell_models == "True")$real_mcc, 
+  y=dplyr::filter(basic_data, smell_models == "False")$real_mcc, 
+  alternative="greater")
+reproducer::PHat.test(
+  x=dplyr::filter(basic_data, smell_models == "True")$real_precision, 
+  y=dplyr::filter(basic_data, smell_models == "False")$real_precision, 
+  alternative="greater")
+
+print("Single metric - code smells use or no")
+
+reproducer::PHat.test(
+  x=dplyr::filter(single_data, smell_models == "True")$real_mcc, 
+  y=dplyr::filter(single_data, smell_models == "False")$real_mcc, 
+  alternative="greater")
+reproducer::PHat.test(
+  x=dplyr::filter(single_data, smell_models == "True")$real_precision, 
+  y=dplyr::filter(single_data, smell_models == "False")$real_precision, 
+  alternative="greater")
+
+
+print("Minimal sets - 7E vs 9B")
+
+reproducer::PHat.test(
+  x=dplyr::filter(minimal_data, metric_set == "7E")$real_mcc, 
+  y=dplyr::filter(minimal_data, metric_set == "9B")$real_mcc, 
+  alternative="greater")
+reproducer::PHat.test(
+  x=dplyr::filter(minimal_data, metric_set == "7E")$real_precision, 
+  y=dplyr::filter(minimal_data, metric_set == "9B")$real_precision, 
+  alternative="greater")
+
+
+
+
+
+
 
 
